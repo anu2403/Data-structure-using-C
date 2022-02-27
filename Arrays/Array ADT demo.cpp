c4a4d794d13dbaafb8c6502333528203fda55836
Annapurna Tripathi <98078834+anu2403@users.noreply.github.com>
2022-02-27 09:40:25 +0530

    Add files via upload

---
+++ Arrays/Array ADT demo.cpp
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Array 
+{
+	int *A;
+	int size;
+	int length;
+
+};
+void  display (struct Array arr)
+
+{
+	int i;
+	printf("elements are \n");
+	for(i=0;i<arr.length;i++)
+	printf("%d ",arr.A[i]);
+}
+int main()
+{
+	struct Array arr;
+	int i, n;
+	printf("Enter the size of array");
+	scanf("%d",&arr.size);
+	arr.A=(int*)malloc(arr.size*sizeof(int));
+	arr.length=0;
+	printf("Enter the number of numbers");
+	scanf("%d",&n);
+	printf("Enter all elements\n");
+	for(i=0;i<n;i++)
+	scanf("%d",&arr.A[i]);
+	arr.length=n;
+	display(arr);
+	return 0;
+	
+}
